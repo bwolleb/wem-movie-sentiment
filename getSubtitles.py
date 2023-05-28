0332a5cb28f8252de5b93abf0466654dfe5137d2
Benoist Wolleb <benoist.wolleb@la-cave.ch>
2023-05-28 11:09:32 +0200

    Improve a bit getImages and getSubtitles scripts

--- getSubtitles.py
+++ getSubtitles.py
@@ -9,7 +9,7 @@ def main():
 	parser = argparse.ArgumentParser("Subtitles downloader")
 	parser.add_argument("movies", help="Movies json")
 	parser.add_argument("path", help="Where to save images")
-	parser.add_argument("--sleep", type=float, required=False, help="TIme to sleep between requests (seconds)", default=0)
+	parser.add_argument("--sleep", type=float, required=False, help="Time to sleep between requests (seconds)", default=0)
 	args = parser.parse_args()
 
 	f = open(args.movies)
@@ -23,9 +23,11 @@ def main():
 		srtPath = os.path.join(args.path, movie["uuid"] + ".srt")
 		if os.path.isfile(srtPath):
 			# Subtitles were already downloaded
+			print("Skip", movie["title"])
 			continue
 		cleanTitle = re.sub('[^\w ]', '', movie["title"])
 		try:
+			print("Trying", movie["title"])
 			subs = scrapSubs.getSubtitles(cleanTitle, movie["year"], 1, False)
 			if len(subs) > 0:
 				w = open(srtPath, "wb")
@@ -34,9 +36,8 @@ def main():
 				ok += 1
 				if args.sleep > 0:
 					time.sleep(args.sleep)
-		except:
-			pass
-		if ok == 1: break
+		except Exception as e:
+			print("Request failed for movie", movie["title"], str(e))
 	print("Downloaded", ok, "on", len(movies))
 
 if __name__ == '__main__':
